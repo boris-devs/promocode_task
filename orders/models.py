0db039927244d8d211d4f74770415be6a341bed3
xborismenx <boris.tikhonov.21@gmail.com>
2026-03-31 19:56:55 +0300

    Rename `total_discount` to `discount` in Order model and add validators

--- orders/models.py
+++ orders/models.py
@@ -17,6 +17,6 @@ class Order(models.Model):
     promo_code = models.ForeignKey("promocodes.Promocode", on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    total_discount = models.IntegerField(help_text="total discount in percent", default=0,
+    discount = models.IntegerField(help_text="total discount in percent", default=0,
                                          validators=[MinValueValidator(0), MaxValueValidator(100)])
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
